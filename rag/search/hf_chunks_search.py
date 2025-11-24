dedee6e47c2d99b5bfa17478dad1db5b16c7b0b7
Srikanth Pragada <srikanthpragada@gmail.com>
2025-11-24 09:03:39 +0530

    24-NOV

--- rag/search/hf_chunks_search.py
+++ rag/search/hf_chunks_search.py
@@ -3,7 +3,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-loader = PyPDFLoader("../docs/courses_offered.pdf", mode='page')
+loader = PyPDFLoader("./docs/courses_offered.pdf", mode='page')
 docs = loader.load()
 print("Loaded documents", len(docs))
 
